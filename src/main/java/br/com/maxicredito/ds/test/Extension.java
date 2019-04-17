be789d615666a815003ad90bf29f1263ec306fa3
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2019-04-17 08:20:15 -0300

    temp

--- src/main/java/br/com/maxicredito/ds/test/Extension.java
+++ src/main/java/br/com/maxicredito/ds/test/Extension.java
@@ -7,19 +7,21 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import javax.enterprise.event.Observes;
-import javax.enterprise.inject.spi.BeforeBeanDiscovery;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
 
 public class Extension implements javax.enterprise.inject.spi.Extension {
 	private static final Logger logger = Logger.getLogger(Extension.class.getName());
-	private final List<Step> steps = new LinkedList<Step>();
-	
-	public Extension() {
-		steps.add(new JndiStep());
-		steps.add(new DsStep());
-		steps.add(new JtaStep());
-	}
+	private final List<Step> steps = new LinkedList<Step>() {
+		private static final long serialVersionUID = -519249438341601688L;
 
-	public void executar(final @Observes BeforeBeanDiscovery bdv) {
+		{
+			add(new JndiStep());
+			add(new DsStep());
+			add(new JtaStep());
+		}
+	};
+
+	public void run(@Observes final AfterBeanDiscovery abd) {
 		logger.log(FINE, "Iniciando criação de JNDI Server, DataSources e Contexto JTA");
 		for (final Step step : steps) {
 			step.run();
--- src/test/java/br/com/maxicredito/ds/test/ExtensionTest.java
+++ src/test/java/br/com/maxicredito/ds/test/ExtensionTest.java
@@ -1,54 +1,49 @@
 package br.com.maxicredito.ds.test;
 
+import static java.lang.Boolean.FALSE;
+
 import java.sql.Connection;
 import java.sql.Statement;
 
-import javax.enterprise.inject.Produces;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 import javax.transaction.TransactionManager;
 
-import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
+import org.jboss.weld.junit5.EnableWeld;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(CdiTestRunner.class)
+@EnableWeld
 public class ExtensionTest {
 	private InitialContext context;
 	private final String dsName = "java:/jdbc/datasources/MyDS";
 	private final String dsName2 = "jdbc/MyDS2";
 	private final String dsNameApp = "java:/app/env/jdbc/datasources/MyDS";
 	private final String tmName = "java:/comp/TransactionManager";
-
+	
 	@Before
 	public void setUp() throws NamingException {
+		new Extension().run(null);
 		context = new InitialContext();
-	}
-	
-	@Produces
-	public EntityManager getEntityManager(){
-		return null;
 	}
 
 	@Test
 	public void test() throws Exception {
 		final DataSource ds = (DataSource) context.lookup(dsName);
 		final Connection connection = ds.getConnection();
-		connection.setAutoCommit(Boolean.FALSE);
+		connection.setAutoCommit(FALSE);
 		final Statement statement = connection.createStatement();
 		final String sql = "CREATE TABLE IF NOT EXISTs test(id integer)";
 		statement.execute(sql);
 		connection.commit();
 	}
-	
+
 	@Test
 	public void testApp() throws Exception {
 		final DataSource ds = (DataSource) context.lookup(dsNameApp);
 		final Connection connection = ds.getConnection();
-		connection.setAutoCommit(Boolean.FALSE);
+		connection.setAutoCommit(FALSE);
 		final Statement statement = connection.createStatement();
 		final String sql = "CREATE TABLE IF NOT EXISTs test(id integer)";
 		statement.execute(sql);
@@ -66,7 +61,7 @@ public class ExtensionTest {
 	public void test2() throws Exception {
 		final DataSource ds = (DataSource) context.lookup(dsName2);
 		final Connection connection = ds.getConnection();
-		connection.setAutoCommit(Boolean.FALSE);
+		connection.setAutoCommit(FALSE);
 		final Statement statement = connection.createStatement();
 		final String sql = "CREATE TABLE IF NOT EXISTs test(id integer)";
 		statement.execute(sql);
